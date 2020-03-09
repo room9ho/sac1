03990ea3cc16d3243bf768fc1db480d2275aeef9
Kenny Lee Sin Cheong <kenny.lee@redhat.com>
2020-03-09 15:19:22 -0400

    Reverts b63caaa2 and replaces basestring for str

--- util/jsontemplate.py
+++ util/jsontemplate.py
@@ -1,8 +1,6 @@
 import json
 import re
 
-from six import string_types
-
 from jsonpath_rw import parse as parse_json_path
 from jsonpath_rw.lexer import JsonPathLexerError
 
@@ -30,51 +28,16 @@ class JSONTemplate(object):
         self.apply({})
 
     def apply(self, data):
-        return self._apply(self._parsed, data)
-
-    def _apply(self, obj, data):
-        if isinstance(obj, string_types):
-            return self._process_string(obj, data)
-        elif isinstance(obj, dict):
-            return {
-                self._process_string(key, data): self._apply(value, data)
-                for key, value in obj.items()
-            }
-        elif isinstance(obj, list):
-            return [self._apply(item, data) for item in obj]
-        else:
-            return obj
-
-    def _process_string(self, str_value, data):
-        # Check for a direct match first.
-        if re.match("^" + INLINE_PATH_PATTERN + "$", str_value):
-            expression = str_value[2:-1]
-            return self._process_inline(expression, data)
-
-        def process_inline(match):
-            result = self._process_inline(match.group(1), data)
-            if result is None:
-                return "(none)"
-
-            if isinstance(result, list) and len(result) > 1:
-                return ",".join(result)
-
-            return str(result)
-
-        return re.sub(INLINE_PATH_PATTERN, process_inline, str_value)
-
-    def _process_inline(self, expression, data):
-        if not expression:
-            raise JSONTemplateParseException("Empty expression found")
+        return apply_data_to_obj(self._parsed, data)
 
 
 def apply_data_to_obj(obj, data, missing="(none)"):
-    if isinstance(obj, basestring):
+    if isinstance(obj, str):
         return _process_string(obj, data, missing)
     elif isinstance(obj, dict):
         return {
             _process_string(key, data, missing): apply_data_to_obj(value, data, missing)
-            for key, value in obj.iteritems()
+            for key, value in obj.items()
         }
     elif isinstance(obj, list):
         return [apply_data_to_obj(item, data, missing) for item in obj]
